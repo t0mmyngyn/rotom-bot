56a035d16024182b6803f68e6affcba4d4f373fc
t0mmyngyn <s3845257@student.rmit.edu.au>
2023-09-18 19:30:01 +1000

    Fixed Command and Slash Handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('../config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-
-// Retrieve Command Files for bot startup
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -18,7 +15,6 @@ for (const folder of commandFolders) {
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
 		} else {
@@ -27,18 +23,17 @@ for (const folder of commandFolders) {
 	}
 }
 
-// Receiving command interactions
+client.once(Events.ClientReady, () => {
+	console.log('Ready!');
+});
+
 client.on(Events.InteractionCreate, async interaction => {
-    
-    if (!interaction.isChatInputCommand()) return;
+	if (!interaction.isChatInputCommand()) return;
 
-    const command = interaction.client.commands.get(interaction.commandName);
+	const command = client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-xw
+	if (!command) return;
+
 	try {
 		await command.execute(interaction);
 	} catch (error) {
@@ -49,15 +44,6 @@ xw
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 		}
 	}
-    
 });
 
-
-// Start Bot
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-
-// Log in to Discord with Client Token
 client.login(token);
